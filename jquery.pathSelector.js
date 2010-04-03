1445ed692d7f784fdb7a6e70b7db84f4df790bed
hmvictor <hmvictor@localhost>
2010-04-02 22:57:43 -0600

    Version 1.2

--- jquery.pathSelector.js
+++ jquery.pathSelector.js
@@ -24,71 +24,70 @@
         });
     };
 	
-	$.fn.pathSelectorAjax = function(options, url) {
+    $.fn.pathSelectorAjax = function(options, url) {
         return this.each(function(index, element) {
             makePathSelector(element, options, true, url);
         });
     };
 	
-	$.fn.pathSelectorFixedOptions = function(optionsArray, options) {
+    $.fn.pathSelectorFixedOptions = function(optionsArray, options) {
         return this.each(function(index, element) {
             makePathSelector(element, options, false, function(value, subvalues){
-				return optionsArray[subvalues.length];
-			});
+                return optionsArray[subvalues.length];
+            });
         });
     };
 	
-	$.fn.pathSelectorDynamicOptions = function(optionsFunc, options) {
+    $.fn.pathSelectorDynamicOptions = function(optionsFunc, options) {
         return this.each(function(index, element) {
             makePathSelector(element, options, false, optionsFunc);
         });
     };
 	
-	$.fn.pathSelectorAjaxOptions = function(url, options) {
+    $.fn.pathSelectorAjaxOptions = function(url, options) {
         return this.each(function(index, element) {
             makePathSelector(element, options, true, url);
         });
     };
 	
-	
 
-        function makePathSelector(input, options, isAjax, urlOrFunc) {
-            if(input.name == "input"){
-                return;
-            }
-            var defaults={
-                separator: "."
-            };
-            options = $.extend({}, defaults, options);
-
-
-            var jInput=$(input);
-            jInput.css("display", "none");
-            jInput.after("<span class='pathSelector'></span>");
-            
-            var jSpan=jInput.next("span");
-            jInput.remove();
-            jSpan.append(input);
-            var pathSelector=jSpan.get(0);
-            jSpan.append("<ul class='contextMenu'></ul>");
-            pathSelector.options=options;
-            pathSelector.fetchOptions=fetchOptions;
-            pathSelector.getAndShowOptions=getAndShowOptions;
-            pathSelector.setMenuOptions=setMenuOptions;
-            pathSelector.appendLevelSelector=appendLevelSelector;
-            pathSelector.showSelectedValue=showSelectedValue;
-            pathSelector.getSelectedValues=getSelectedValues;
-            pathSelector.cache=new Object();
-            pathSelector.isAjax=isAjax;
-            if(pathSelector.isAjax){
-                pathSelector.url=urlOrFunc;
-            }else{
-                pathSelector.optionsFunc=urlOrFunc;
-            }
-            
-            myWatch(input, "value", valueChanged);
-            $(jInput).val("");
+    function makePathSelector(input, options, isAjax, urlOrFunc) {
+        if(input.name == "input"){
+            return;
         }
+        var defaults={
+            separator: "."
+        };
+        options = $.extend({}, defaults, options);
+
+
+        var jInput=$(input);
+        jInput.css("display", "none");
+        jInput.after("<span class='pathSelector'></span>");
+            
+        var jSpan=jInput.next("span");
+        jInput.remove();
+        jSpan.append(input);
+        var pathSelector=jSpan.get(0);
+        jSpan.append("<ul class='contextMenu'></ul>");
+        pathSelector.options=options;
+        pathSelector.fetchOptions=fetchOptions;
+        pathSelector.getAndShowOptions=getAndShowOptions;
+        pathSelector.setMenuOptions=setMenuOptions;
+        pathSelector.appendLevelSelector=appendLevelSelector;
+        pathSelector.showSelectedValue=showSelectedValue;
+        pathSelector.getSelectedValues=getSelectedValues;
+        pathSelector.cache=new Object();
+        pathSelector.isAjax=isAjax;
+        if(pathSelector.isAjax){
+            pathSelector.url=urlOrFunc;
+        }else{
+            pathSelector.optionsFunc=urlOrFunc;
+        }
+            
+        $(jInput).val("");
+        valueChanged(input, "value", "");
+    }
 
     function valueChanged(input, propName, value){
         /* Fire jQuery Event */
@@ -107,7 +106,9 @@
             callbackWhenFetched(pathSelector.cache[value]);
         }else{
             if(pathSelector.isAjax){
-                $.getJSON(pathSelector.url, {value: value}, function(options){
+                $.getJSON(pathSelector.url, {
+                    value: value
+                }, function(options){
                     pathSelector.cache[value]=options;
                     callbackWhenFetched(pathSelector.cache[value]);
                 });
@@ -125,11 +126,13 @@
                 }
                 var tempOptions=[];
                 $.each(options, function(index, option){
-                        if(option.value){
-                                tempOptions.push(option);
-                        }else{
-                                tempOptions.push({value:option.toString()});
-                        }
+                    if(option.value){
+                        tempOptions.push(option);
+                    }else{
+                        tempOptions.push({
+                            value:option.toString()
+                            });
+                    }
                 });
                 pathSelector.cache[value]=tempOptions
                 callbackWhenFetched(pathSelector.cache[value]);
@@ -164,12 +167,15 @@
                 }
             }
             );
-   
+
         $(pathSelector).append("<span class='level' value='valueX' level='levelX'><a href='#pathLevel'>".replace("valueX", option.value).replace("levelX", level)+option.label+"</a></span>");
         $(pathSelector).find("span.level[level='"+level+"'] a").click(function(event){
             /*window.alert("selected:" + $(this).parent().attr("value"));
             window.alert("html:" + $(this).html());*/
-            pathSelector.showSelectedValue({value:$(this).parent().attr("value"), label:$(this).html()}, $(this).parent().attr("level") );
+            pathSelector.showSelectedValue({
+                value:$(this).parent().attr("value"),
+                label:$(this).html()
+                }, $(this).parent().attr("level") );
         });
         var options=pathSelector.getSelectedValues();
         var valueString="";
@@ -180,42 +186,44 @@
             valueString += element;
         });
         $(pathSelector).find("input").val(valueString);
+        valueChanged($(pathSelector).find("input").get(0), "value", valueString);
     }
 
     function appendLevelSelector(){
         var pathSelector=this;
         var level=$(pathSelector).find(".arrowButton").length;
         $(pathSelector).append(arrow.replace("levelX", ""+level));
+//        $(pathSelector).find(".arrowButton").find("a").html("&gt;");
         $(pathSelector).find(".arrowButton[level='"+level+"']").contextMenu(
-            {
-                menu: $(pathSelector).find(".contextMenu").get(0),
-                afterHiding: function(){
-                    //$(pathSelector).find("img").attr("src", imageDir+"/arrowrgt.gif");
-                    $(pathSelector).find("a").removeClass("pressed");					
-                },
-                menuShown:function(arrowElement){
-                    pathSelector.clickedArrow=arrowElement;
-                    //$(arrowElement).find("img").attr("src", imageDir+"/arrowdwn.gif");
-                    $(arrowElement).find("a").addClass("pressed");
-                    var value=$(pathSelector).find("input").val();
-                    var tokens=value.split(pathSelector.options.separator);
-
-                    var level=$(arrowElement).attr("level");
-                    var tempValue="";
-                    for(var i=0; i< level; i++){
-                        if(i > 0){
-                            tempValue+=pathSelector.options.separator;
-                        }
-                        tempValue+=tokens[i];
-                    }
-                    pathSelector.getAndShowOptions(tempValue);
-                }
+        {
+            menu: $(pathSelector).find(".contextMenu").get(0),
+            afterHiding: function(){
+                $(pathSelector).find("a").removeClass("pressed");
+//                $(pathSelector).find(".arrowButton").find("a").html("&gt;");
             },
-            function(option, el, position){
-                var level=$(el).attr("level");
-                pathSelector.showSelectedValue(option, level);
+            menuShown:function(arrowElement){
+                pathSelector.clickedArrow=arrowElement;
+                $(arrowElement).find("a").addClass("pressed");
+//                $(arrowElement).find("a").html("*");
+                var value=$(pathSelector).find("input").val();
+                var tokens=value.split(pathSelector.options.separator);
 
+                var level=$(arrowElement).attr("level");
+                var tempValue="";
+                for(var i=0; i< level; i++){
+                    if(i > 0){
+                        tempValue+=pathSelector.options.separator;
+                    }
+                    tempValue+=tokens[i];
+                }
+                pathSelector.getAndShowOptions(tempValue);
             }
+        },
+        function(option, el, position){
+            var level=$(el).attr("level");
+            pathSelector.showSelectedValue(option, level);
+
+        }
         );
         $(pathSelector).attr("levelCount", $(".arrowButton").length);
     }
@@ -235,21 +243,6 @@
         });
         $(this).find(".contextMenu").html(html);
         $(this).find(".contextMenu").get(0).proccessHTML();
-    }
-
-    function myWatch(object, propertyName, myFunction){
-        if(object.watch){
-            object.watch(propertyName, function(propName, oldVal, newVal){
-                myFunction(object, propName, newVal);
-                return newVal;
-            });
-        }else{
-            object.onpropertychange=function(){
-                if(event.propertyName == propertyName){
-                    myFunction(event.srcElement, event.propertyName, event.srcElement[event.propertyName]);
-                }
-            };
-        }
     }
 
 })(jQuery);
